0bfc892d2b7ad01c58bb95ac0aa0dae1b9b48724
John Chen <johnchen456@gmail.com>
2019-11-05 15:11:38 -0500

    test: Added ubuntu1804 test

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -1,1 +1,6 @@
 # frozen_string_literal: true
+
+describe command('faketime --version') do
+  its('stdout') { should eq "\nfaketime: Version 0.9.7\nFor usage information please use 'faketime --help'.\n" }
+  its('exit_status') { should eq 0 }
+end
