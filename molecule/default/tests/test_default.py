a3f90eb1bbe809e4199ae96ebd93c537b960b2b4
BrianM <briananthonymccarthy@gmail.com>
2020-04-13 09:46:48 -0400

    feat: Add ubuntu 20.04 platforms

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -3,14 +3,17 @@ import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
 def test_faketime(host):
     cmd = host.run("faketime --version")
     assert cmd.succeeded
-    assert cmd.stdout == """
+    assert (
+        cmd.stdout
+        == """
 faketime: Version 0.9.7
 For usage information please use 'faketime --help'.
 """
+    )
